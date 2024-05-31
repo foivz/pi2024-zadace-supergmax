0cd1e169f6fe7af230de62da35fec007abb5a45f
SuperGmaX <gmax9292@gmail.com>
2024-05-31 18:54:15 +0200

    add exemple software
    add template software done in class

---
+++ Software/FrmLogin.cs
@@ -0,0 +1,53 @@
+﻿using Evaluation_Manager.Models;
+using Evaluation_Manager.Repositories;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Evaluation_Manager
+{
+    public partial class FrmLogin : Form
+    {
+        public static Teacher LoggedTeacher { get; private set; }
+
+        public FrmLogin()
+        {
+            InitializeComponent();
+        }
+
+        private void btnLogin_Click(object sender, EventArgs e)
+        {
+            if (txtUsername.Text == "")
+            {
+                MessageBox.Show("Username was not entered!", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            else if (txtPassword.Text == "")
+            {
+                MessageBox.Show("Password was not entered!", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            else
+            {
+
+                var teacher = TeacherRepository.GetTeacher(txtUsername.Text);
+
+                if (teacher != null && txtPassword.Text == teacher.Password)
+                {
+                    FrmStudents frmStudents = new FrmStudents();
+                    Hide();
+                    frmStudents.ShowDialog();
+                    Close();
+                }
+                else
+                {
+                    MessageBox.Show("Wrong credentials!", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+        }
+    }
+}
---
+++ Software/Repositories/TeacherRepository.cs
@@ -0,0 +1,61 @@
+﻿using DBLayer;
+using Evaluation_Manager.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Evaluation_Manager.Repositories
+{
+    internal class TeacherRepository
+    {
+        public static Teacher GetTeacher(int id)
+        {
+            string sqlQuery = $"SELECT * FROM Teachers WHERE Id = {id}";
+            return FetchTeacher(sqlQuery);
+        }
+
+        public static Teacher GetTeacher(string username)
+        {
+            string sqlQuery = $"SELECT * FROM Teachers WHERE Username = '{username}'";
+            return FetchTeacher(sqlQuery);
+        }
+        private static Teacher FetchTeacher(string sqlQuery)
+        {
+            DB.OpenConnection();
+            var reader = DB.GetDataReader(sqlQuery);
+            Teacher teacher = null;
+            if (reader.HasRows)
+            {
+                reader.Read();
+                teacher = CreateObject(reader);
+                reader.Close();
+            }
+            DB.CloseConnection();
+            return teacher;
+        }
+
+        private static Teacher CreateObject(SqlDataReader reader)
+        {
+            int id = int.Parse(reader["Id"].ToString());
+            string firstname = reader["FirstName"].ToString();
+            string lastname = reader["LastName"].ToString();
+            string username = reader["Username"].ToString();
+            string password = reader["Password"].ToString();
+
+            var teacher = new Teacher
+            {
+                Id = id,
+                FirstName = firstname,
+                LastName = lastname,
+                Username = username,
+                Password = password
+            };
+            
+            return teacher;
+        }
+    }
+}
